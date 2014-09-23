8d5a85ae40ad662ceb3c53ffaaf4c8055d982433
Kyle Robinson Young <kyle@dontkry.com>
2014-09-22 21:24:56 -0700

    Throw an error if a user home env var is not found. Fixes GH-1

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
 var path = require('path');
 
 module.exports = function() {
-  return path.resolve.apply(path.resolve, [
-    process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']
-  ].concat(Array.prototype.slice.call(arguments, 0)));
+  var home = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+  if (!home) {
+    throw new Error('Could not find a valid user home path.');
+  }
+  return path.resolve.apply(path.resolve, [home]
+    .concat(Array.prototype.slice.call(arguments, 0)));
 };
